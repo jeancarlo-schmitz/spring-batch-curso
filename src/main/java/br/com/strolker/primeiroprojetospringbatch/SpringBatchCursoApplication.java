761e8a8abcac759a6c1bdbd5d928c1686c101e4a
jean.carlo <jean.schmitz@publicacoesonline.com.br>
2023-03-23 14:46:01 -0300

    Subindo alteracoes aula 01

---
+++ src/main/java/br/com/strolker/primeiroprojetospringbatch/SpringBatchCursoApplication.java
@@ -0,0 +1,13 @@
+package br.com.strolker.primeiroprojetospringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/strolker/primeiroprojetospringbatch/SpringBatchCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.strolker.primeiroprojetospringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
